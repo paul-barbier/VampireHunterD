714e654b47054fe63d9ae48bb6f25e3470e24d86
ShinLTM <kilian.sa11@gmail.com>
2025-11-20 15:58:08 +0100

    V6.5
    ...

--- Assets/20_Scripts/CheckPoints.cs
+++ Assets/20_Scripts/CheckPoints.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class CheckPoints : MonoBehaviour
 {
+    private Health Health;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerCharacter controller = collision.GetComponent<PlayerCharacter>();
         if (controller != null)
         {
+            Health._currentHealth = Health._maxHealth;
             controller.checkpoint = this;
         }
     }
