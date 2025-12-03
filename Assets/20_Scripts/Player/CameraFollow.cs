47befabc3a7d663d13232bc13ab4b105d2905431
ShinLTM <kilian.sa11@gmail.com>
2025-12-03 11:31:00 +0100

    CAMERA FINALE V1

--- Assets/20_Scripts/Player/CameraFollow.cs
+++ Assets/20_Scripts/Player/CameraFollow.cs
@@ -7,17 +7,12 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private PlayerCharacter _player;
 
     [Header("Camera Settings")]
-    [SerializeField] private float offsetCam = 3.0f;
-    [SerializeField] private float camLerpSpeed = 5f;
+    [SerializeField] private float offsetCam = 6.0f;
+    [SerializeField] private float camLerpSpeed = 6f;
 
     private Vector3 _targetOffset;
     private Vector2 _targetHardLimit;
 
-    [SerializeField] private float horizontalOffset = 4f;
-    [SerializeField] private float smoothSpeed = 5f;
-
-    private float targetOffsetX = 4f;
-    private float currentOffsetX = 10f;
     private void Awake()
     {
         _player = GetComponent<PlayerCharacter>();
@@ -30,14 +25,14 @@ public class CameraFollow : MonoBehaviour
         if (input > 0.3f)
         {
             _targetOffset = new Vector3(offsetCam, 1f, 0f);
-            _targetHardLimit = new Vector2(-1f, 0f);
+            _targetHardLimit = new Vector2(-0.7f, 0f);
         }
         else if (input < -0.3f)
         {
             _targetOffset = new Vector3(-offsetCam, 1f, 0f);
-            _targetHardLimit = new Vector2(1f, 0f);
+            _targetHardLimit = new Vector2(0.7f, 0f);
         }
-        else
+        else if (input == 0)
         {
             _targetOffset = new Vector3(0f, 1f, 0f);
             _targetHardLimit = Vector2.zero;
@@ -45,17 +40,16 @@ public class CameraFollow : MonoBehaviour
 
         _camera.TargetOffset = Vector3.Lerp(_camera.TargetOffset, _targetOffset, Time.deltaTime * camLerpSpeed);
 
-        _camera.Composition.HardLimits.Offset = Vector2.Lerp(_camera.Composition.HardLimits.Offset,_targetHardLimit,Time.deltaTime * camLerpSpeed);
+        _camera.Composition.HardLimits.Offset = Vector2.Lerp(_camera.Composition.HardLimits.Offset, _targetHardLimit, Time.deltaTime * camLerpSpeed);
     }
 
     public void LockCamOnPlayer()
     {
-        _camera.Composition.DeadZone.Size = new Vector2(-0.4f, 0f);
+        _camera.Composition.DeadZone.Size = new Vector2(0.6f, 0f);
     }
 
     public void UnLockCamOnPlayer()
     {
-
-        _camera.Composition.DeadZone.Size = new Vector2(-0.4f, 0.45f);
+        _camera.Composition.DeadZone.Size = new Vector2(0.6f, 1f);
     }
 }
