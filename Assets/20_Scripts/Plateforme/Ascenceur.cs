40bbac85c7602f8325869d2dbcc2a77601e8783c
Nitropical <paulbarbier295@gmail.com>
2025-12-08 11:41:48 +0100

    Update Ascenceur

--- Assets/20_Scripts/Plateforme/Ascenceur.cs
+++ Assets/20_Scripts/Plateforme/Ascenceur.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 
 public class Ascenceur : MonoBehaviour
 {
+
+    PlayerCharacter character;
+
     public Transform posA, posB;
     public float speed;
     private Vector3 targetPos;
+    private bool disableMovement = false;
     [SerializeField] private float WaitingExitTime;
 
-    private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    private void Start()
+    {
+        transform.position = posA.position;
+    }
+
+    private void Update()
+    {
+        if (disableMovement)
+        {
+            character.DisableAllMovement();
+        }
+    }
+
+    private void OnTriggerStay2D(UnityEngine.Collider2D collision)
     {
         Debug.Log("PlayerSurAscenceur");
         collision.transform.SetParent(transform);
-        if (Vector2.Distance(transform.position, posA.position) < 0.1f)
-        {
-            targetPos = posB.position;
-        }
+        targetPos = posB.position;
         transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-    }
-    private void OnTriggerExit2D(UnityEngine.Collider2D collision)
-    {
-        Debug.Log("PlayerPasSurAscenceur");
-        StartCoroutine(ElevatorWaiting());
-        targetPos = posA.position;
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-    }
-
-    private IEnumerator ElevatorWaiting()
-    {
-        yield return new WaitForSeconds(WaitingExitTime);
+        disableMovement = true;
     }
 }
