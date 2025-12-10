b04d6073128d214deb79e13501b94a014978bb3f
Hymagift <pierre.louis.brel@gmail.com>
2025-12-10 10:07:36 +0100

    DDeath+ Cercueil CheckpointAnimated+ScreamBackground

--- Assets/20_Scripts/Player/CheckPoints.cs
+++ Assets/20_Scripts/Player/CheckPoints.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class CheckPoints : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.CompareTag("Player"))
         {
+            animator.SetBool("PlayAnime", true);
             Health playerHealth = collision.GetComponent<Health>();
 
             playerHealth.GetHeal(playerHealth.GetMaxHealth());
