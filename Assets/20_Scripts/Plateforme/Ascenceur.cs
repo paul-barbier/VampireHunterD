dac368bf936921e80265aa902717144164622098
ShinLTM <kilian.sa11@gmail.com>
2025-12-18 15:18:44 +0100

    Push Miroir (YEN A 2 DANS LE PREFAB ILS SONT BON) et push Ascenceur

--- Assets/20_Scripts/Plateforme/Ascenceur.cs
+++ Assets/20_Scripts/Plateforme/Ascenceur.cs
@@ -5,39 +5,40 @@ using UnityEngine;
 public class Ascenceur : MonoBehaviour
 {
 
-    PlayerCharacter character;
+    [SerializeField] private PlayerCharacter character;
 
     public Transform posA, posB;
     public float speed;
     private Vector3 targetPos;
-    private bool disableMovement = false;
+
     [SerializeField] private float WaitingExitTime;
     [SerializeField] private Collider2D ascenceurCollider;
     [SerializeField] private GameObject ColliderMur;
-    private bool onPlateforme = false;
+    [SerializeField] private Animator AnimAscenceur;
 
     private void Start()
     {
+        AnimAscenceur = GetComponent<Animator>();
         character = FindAnyObjectByType<PlayerCharacter>();
         transform.position = posA.position;
         ColliderMur.SetActive(false);
     }
 
-    private void Update()
-    {
-    }
-
     private void OnTriggerStay2D(UnityEngine.Collider2D collision)
     {
-        collision.GetComponent<PlayerCharacter>();
+        character = collision.GetComponent<PlayerCharacter>();
+        character._movementDisabled = true;
         Debug.Log("PlayerSurAscenceur");
         collision.transform.SetParent(transform);
         targetPos = posB.position;
         transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         ColliderMur.SetActive(true);
+        AnimAscenceur.SetBool("OuvreAscenceur", false);
         if (Vector2.Distance(transform.position, targetPos) < 1f)
         {
+            AnimAscenceur.SetBool("OuvreAscenceur", true);
             Debug.Log("Ascenceur arrivé");
+            character._movementDisabled = false;
             ascenceurCollider.enabled = false;
             ColliderMur.SetActive(false);
         }
