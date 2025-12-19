6d2c1cfeea5645f7e0e47690ec80aacf9cc49057
Creatinus <thibaud.cambuzat@outlook.fr>
2025-12-19 11:02:27 +0100

    grdgwdr

--- Assets/20_Scripts/TransitionTour.cs
+++ Assets/20_Scripts/TransitionTour.cs
@@ -4,11 +4,17 @@ public class TransitionTour : MonoBehaviour
 {
     [SerializeField] private GameObject _plafond;
     [SerializeField] private GameObject _murQuiBloque;
+    [SerializeField] private GameObject _GradientCatac;
+    [SerializeField] private GameObject _GradientBlack;
+    [SerializeField] private GameObject _GradientCatacombes;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(_plafond);
         Destroy(_murQuiBloque);
+        Destroy(_GradientCatac);
+        Destroy(_GradientBlack);
+        Destroy(_GradientCatacombes);
         Object.FindFirstObjectByType<CaveDarkness3D>().RestoreLight();
     }
 }
