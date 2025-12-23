cfe318bdee3486e38054aad7f3030f781cc8914d
Hymagift <pierre.louis.brel@gmail.com>
2025-12-23 21:01:56 +0100

    zeze

--- Assets/90_SFX/SoundScripts/Obejct_Sound.cs
+++ Assets/90_SFX/SoundScripts/Obejct_Sound.cs
@@ -10,7 +10,7 @@ public class Obejct_Sound : MonoBehaviour
         collision.gameObject.GetComponent<PlayerCharacter>();
         if (collision && AlreadyPlayed == false)
         { 
-            AudioSource.PlayClipAtPoint(SoundManager.instance.soundList[(int)SoundType.BatVFX].Sounds[0], transform.position, 1f);
+            AudioSource.PlayClipAtPoint(SoundManager.instance.soundList[(int)SoundType.BatVFX].Sounds[0], transform.position, 0.4f);
             AlreadyPlayed = true;
         }
 
